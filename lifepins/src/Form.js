1de0e9363adf6455a0dd52f1c358bb42e4655e80
Rio <rnose512@gmail.com>
2017-09-16 15:34:10 -0700

    Fix calling components in app

---
+++ lifepins/src/Form.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Form extends Component {
+  render() {
+    return (
+      <div className="Form">
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default Form;
---
+++ lifepins/src/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+				<p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
