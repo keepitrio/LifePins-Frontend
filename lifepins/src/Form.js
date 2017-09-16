2ddcc3b3ced5b01643a04dc1bbc6f79c0b4d1200
Rio <rnose512@gmail.com>
2017-09-16 16:52:36 -0700

    Create form

--- lifepins/src/Form.js
+++ lifepins/src/Form.js
@@ -1,13 +1,85 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dropdown from 'react-dropdown';
 
 class Form extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			name: '',
+			contact: '',
+			address: '',
+			category: '',
+			numberOfPeople: '',
+			value: 'Category'
+		}
+
+		this.handleInputChange = this.handleInputChange.bind(this);
+	}
+
+	handleInputChange = (e) => {
+		const value = e.target.value;
+		const name = e.target.name;
+
+		this.setState({
+			[name]: value
+		});
+	}
+
   render() {
     return (
-      <div className="Form">
-        <p>Hello</p>
-      </div>
+      <form>
+      	<label>How can you help?</label>
+      	<label>
+      			<input
+      				placeholder="Name:"
+      				name="name"
+      				type="string"
+      				onChange={this.handleInputChange}
+      			/>
+      	</label>
+      	<br />
+      	<label>
+      			<input
+      				placeholder="Contact Info (Phone number or email):"
+      				name="contact"
+      				type="string"
+      				onChange={this.handleInputChange}
+      			/>
+      	</label>
+      	<br />
+      	<label>
+      			<input
+      				placeholder="Address:"
+      				name="address"
+      				type="string"
+      				onChange={this.handleInputChange}
+      			/>
+      	</label>
+      	<br />
+      	<label>
+      		<label> What you can provide: </label>
+    			<select 
+    				value={this.state.value}
+    				onChange={this.handleInputChange}>
+    				<option value="water">Water</option>
+    				<option value="food">Food</option>
+    				<option value="shower">Shower</option>
+    				<option value="bed">Bed</option>
+  				</select>
+      	</label>
+      	<br />
+      	<label>
+      			<input
+      				placeholder="Number of people you can provide for:"
+      				name="numberOfPeople"
+      				type="string"
+      				onChange={this.handleInputChange}
+      			/>
+      	</label>
+      </form>
     );
   }
 }
