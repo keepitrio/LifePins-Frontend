39cf1f705eab533850a844cc229c350dc18f6ea6
Yun Jae Chang <yunjae0500@yahoo.com>
2017-09-16 18:34:39 -0700

    Get multiple green markers to show

--- lifepins/public/map.js
+++ lifepins/public/map.js
@@ -1,12 +1,32 @@
 window.onload = function() {
 
-L.mapquest.key = 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24';
+  L.mapquest.key = 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24';
 
-var map = L.mapquest.map('map', {
-  center: [37.7749, -122.4194],
-  layers: L.mapquest.tileLayer('map'),
-  zoom: 12
-});
+  var map = L.mapquest.map('map', {
+    center: [40.7392, -104.9903],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12
+  });
 
-map.addControl(L.mapquest.control());
+  var addressPoints = [
+  [40.7392, -104.9903],
+  [41.7392, -104.9903],
+  [42.7392, -104.9903],
+  [43.7392, -104.9903],
+  [44.7392, -104.9903]
+  ]
+
+  for (var i = 0; i < addressPoints.length; i++) {
+    L.marker(addressPoints[i], {
+      icon: L.mapquest.icons.marker({
+        primaryColor: '#474747',
+        secondaryColor: '#228B22',
+        shadow: true,
+        size: 'md'
+      }),
+      draggable: true
+    }).bindPopup(`sdfa <br/> sdf`).addTo(map);
+  }
+
+  map.addControl(L.mapquest.control());
 }
